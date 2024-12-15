3b742b086ca151802d82df99bc65892ff6d2bc34
Michel Morais <valentimmichel@gmail.com>
2024-12-15 17:40:51 -0300

    Reference method with static method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,4 @@
-//Interface implementation
+//Reference method with static method
 package application;
 
 import java.util.ArrayList;
@@ -16,7 +16,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(new ConsumerProduct());
+		list.forEach(Product::staticConsumerProoduct);
 		
 		list.forEach(System.out::println);
 
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -26,6 +26,10 @@ public class Product {
 		this.price = price;
 	}
 	
+	public static void staticConsumerProoduct(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
+	}
+	
 	@Override
 	public String toString() {
 		return "Product [name = " + name + ", price = " + String.format("%.2f", price) + "]";
